996482ceb09586275f301957165b882998231f99
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-08-06 14:58:14 +0900

    v3-2

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import Http404
 
+from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
