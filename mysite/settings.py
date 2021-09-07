e46a0bbf89449dcc3563fd43c1ce71e12f06d6d9
yoshifumi watanabe <yoshifumi_watanabe_aa@mail.toyota.co.jp>
2021-09-07 13:59:07 +0900

    v7_d1

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+import django_heroku
+
+if 'DYNO' in os.environ:
+    django_heroku.settings(locals())
